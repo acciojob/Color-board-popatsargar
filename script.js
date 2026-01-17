4feb799fd845b84b17422601cbf2d610b774affd
Popat Bhausaheb Sargar <sargarpopat219@gmail.com>
2026-01-17 12:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+const SQUARES = 800;
+
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = getRandomColor();
+    });
+
+    square.addEventListener("mouseout", () => {
+        setTimeout(() => {
+            square.style.backgroundColor = "#1d1d1d";
+        }, 1000);
+    });
+
+    container.appendChild(square);
+}
+
+function getRandomColor() {
+    return `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
