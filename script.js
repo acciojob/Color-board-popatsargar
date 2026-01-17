aa8f6aa9de25d1474c6c8fc952d4de240f302caa
Popat Bhausaheb Sargar <sargarpopat219@gmail.com>
2026-01-17 12:55:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-//your JS code here. If required.
-const container = document.getElementById("container");
-const SQUARES = 800;
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".container");
+    const SQUARES = 800;
 
-for (let i = 0; i < SQUARES; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+    for (let i = 0; i < SQUARES; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
 
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = getRandomColor();
-    });
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = getRandomColor();
+        });
 
-    square.addEventListener("mouseout", () => {
-        setTimeout(() => {
-            square.style.backgroundColor = "#1d1d1d";
-        }, 1000);
-    });
+        square.addEventListener("mouseout", () => {
+            
+            square.style.backgroundColor = "rgb(29, 29, 29)";
+        });
 
-    container.appendChild(square);
-}
+        container.appendChild(square);
+    }
+});
 
 function getRandomColor() {
     return `hsl(${Math.random() * 360}, 100%, 50%)`;
